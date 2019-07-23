d5dd2a43e5beaa16667e5fa3fcc5bcf053111508
Shehan Munasinghe <shehanmunasinghe@gmail.com>
2019-07-23 23:19:12 +0530

    Line follow before ramp
    Line follow before ramp - Need to improve the coin area

--- decide.ino
+++ decide.ino
@@ -28,13 +28,103 @@ void decide(){
     //2
     case STATE_LINE_1:
       Serial2.println("STATE_LINE_1");
-      test_line_follow(); //Inside this function, it will move to Coin Collect state  //robot_state=?? To be updated inside the line follow function
+      test_line_follow_1(); //Inside this function, it will move to Coin Collect state  //robot_state=?? To be updated inside the line follow function
       break;
     //3
     case STATE_DETECTED_COIN_BOX_1:
-      robot_state = STATE_PAUSED; //Change this line
-      break;
+		motors_brake();delay(1000);
+		//Detect Coin Color
+		detected_coin_color=COIN_COLOR_BLUE; //TODO : Write Function to get this from raspberry pi
+		Serial2.println("STATE_DETECTED_COIN_BOX_1");
+		coin_arm_pickup_position();
+		delay(1000);
+		motors_DriveGivenDistance(12);
+		robot_state=STATE_LINE_FOLLOW_UNTIL_Y_JUNCTION_1;
+    	break;
+		
+	case STATE_LINE_FOLLOW_UNTIL_Y_JUNCTION_1:
+		Serial2.println("STATE_LINE_FOLLOW_UNTIL_Y_JUNCTION_1");
+		// test_line_follow_2();
+		while (true)
+		{
+			if(!test_line_follow_3()){
+				robot_state = STATE_DETECTED_Y_JUNCTION;
+				break;
+			}
+		}
 
+		break;
+
+	case STATE_DETECTED_Y_JUNCTION:
+		Serial2.println("STATE_DETECTED_Y_JUNCTION");
+		// motors_brake();delay(1000);	
+
+		switch (detected_coin_color){
+			case COIN_COLOR_GREEN:
+				motors_DriveGivenDistance(13);	
+				while (true)
+				{
+					if(!test_line_follow_3()){
+						robot_state = STATE_DETECTED_COIN_BOX_2;
+						break;
+					}
+				}
+				break;
+			case COIN_COLOR_RED:
+				motors_DriveGivenDistance(13);	
+				motors_Turn_45_Right();
+				//TODO			
+				break;
+			case COIN_COLOR_BLUE:
+				motors_DriveGivenDistance(13);	
+				motors_Turn_45_Left();
+				// motors_DriveGivenDistance(3);	
+				while (true)
+				{
+					// get_IR_readings();get_IR_binary_array();
+					// print_IR_binary_array();
+					// pid_line_follow_step();
+					if(!test_line_follow_3()){
+						robot_state = STATE_DETECTED_COIN_BOX_2;
+						break;
+					}					
+				}	
+				break;					
+			default:
+				break;
+		}
+		break;
+
+    case STATE_DETECTED_COIN_BOX_2:
+		motors_brake();delay(2000);
+		Serial2.println("STATE_DETECTED_COIN_BOX_2");
+		coin_arm_rest_position();
+		motors_DriveGivenDistance(12);
+		motors_brake();delay(1000);
+
+
+		robot_state=STATE_LINE_FOLLOW_UNTIL_Y_JUNCTION_2;
+    	break;
+
+	case STATE_LINE_FOLLOW_UNTIL_Y_JUNCTION_2:
+		// motors_brake();
+		Serial2.println("STATE_LINE_FOLLOW_UNTIL_Y_JUNCTION_2");
+
+		while (true)
+		{
+			if(!test_line_follow_3()){
+				motors_DriveGivenDistance(10);
+				robot_state = STATE_LINE_2;
+				break;
+			}
+		}
+
+		break;
+
+	case STATE_LINE_2:
+		Serial2.println("STATE_LINE_2");
+		test_line_follow_4();//To be changed
+		break;
     //9
     /*case STATE_LINE_2:
       Serial2.println("STATE_LINE_2");
--- ir_check_special.ino
+++ ir_check_special.ino
@@ -19,34 +19,30 @@ int checkIRSpecialCondition(){
     //Serial2.print("L "); Serial2.print(array_sum(irBinaryValue,8,15));
     //Serial2.print("    R ");Serial2.println(array_sum(irBinaryValue,0,7));
     
-    if(array_sum(irBinaryValue,0,15)>=14) //Detect all white T Junction
-    {
-      Serial2.println("IR_ALL_WHITE");
-      return IR_ALL_WHITE;
-    }
-    else if(array_sum(irBinaryValue,0,15)>=7 ) 
-    { motors_brake();
-      get_IR_readings();get_IR_binary_array();
-      delay(100);
+    if(array_sum(irBinaryValue,0,15)>=14){ //Detect all white T Junction
+		Serial2.println("IR_ALL_WHITE");
+		return IR_ALL_WHITE;
+    }else if(array_sum(irBinaryValue,0,15)>=7 ) {
+		motors_brake();
+		get_IR_readings();get_IR_binary_array();
+		delay(100);
      
-      if (array_sum(irBinaryValue,2,13)>=7 && array_sum(irBinaryValue,0,1)<2 && array_sum(irBinaryValue,14,15)<2)
-      {    //Detect coin box
-        Serial2.println("IR_COIN_BOX");
-        return IR_COIN_BOX;
-      }
-      else if(array_sum(irBinaryValue,0,7)>=7 && irBinaryValue[15]==0 && array_sum(irBinaryValue,0,1)>=1) //Detect Right Turn
-      {
-        Serial2.println("Right");
-        return IR_90_RIGHT; 
-      }
-    else if(array_sum(irBinaryValue,8,15)>=7 && irBinaryValue[0]==0 && array_sum(irBinaryValue,14,15)>=1) //Detect Left Turn
-    {
-      // motors_DriveGivenDistance(13);
-      // motors_Turn_90_Left(); Turn left
-      Serial2.println("Left");
-      return IR_90_LEFT;
-    } 
-      
+		if (array_sum(irBinaryValue,2,13)>=7 && array_sum(irBinaryValue,0,1)<2 && array_sum(irBinaryValue,14,15)<2){    
+			//Detect coin box
+			Serial2.println("IR_COIN_BOX");
+			return IR_COIN_BOX;
+		}else if(array_sum(irBinaryValue,0,7)>=7 && irBinaryValue[15]==0 && array_sum(irBinaryValue,0,1)>=1) //Detect Right Turn
+		{
+			Serial2.println("Right");
+			return IR_90_RIGHT; 
+		}
+		else if(array_sum(irBinaryValue,8,15)>=7 && irBinaryValue[0]==0 && array_sum(irBinaryValue,14,15)>=1) //Detect Left Turn
+		{
+		// motors_DriveGivenDistance(13);
+		// motors_Turn_90_Left(); Turn left
+		Serial2.println("Left");
+		return IR_90_LEFT;
+		} 
     }
     /* else if((array_sum(irBinaryValue,2,7)>=3 && array_sum(irBinaryValue,8,13)>=3)  && array_sum(irBinaryValue,0,1)<2 && array_sum(irBinaryValue,14,15)<2 ) //Detect coin box
     {
@@ -81,57 +77,24 @@ int checkIRSpecialCondition(){
     
 }
 
-void check_45()
-{ 
-  int color=1;
-  for(int i=0; i<16; i= i+1){
-            irValue[i]= analogRead(i);        // reads the values of the ir panel
-        }
- 
-  for (int i=0; i<16 ; i=i+1){
-    if (irValue[i]>=450){   //THRESHOLD = 450
-      irBinaryValue[i]= 1;
-      Serial2.print(irBinaryValue[i]);
-      //Serial2.print(" ");
-      }
-    else{
-      irBinaryValue[i]= 0;
-      Serial2.print(irBinaryValue[i]);
-      }
-  }
+int checkIRSpecialConditionInsideCoinArea(){
 
-  if(8<=array_sum(irBinaryValue,0,15)<=14)
-  {
-    fortyJunc(color);
-  }
-  else
-  {
-    return;
-  }
-  
-}
-void fortyJunc(int color)
-{  
-  //012 => BGR => centre, right, left
-  if(color==0)
-  {
-    motors_DriveGivenDistance(15);
-  }
-  else if(color==1)
-  {
-    motors_Turn_45_Right();
-    motors_DriveGivenDistance(15);
-  }
-  else if(color==2)
-  {
-    motors_Turn_45_Left();
-    motors_DriveGivenDistance(15);
-  }
-  else
-  {
-    motors_DriveGivenDistance(15);
-    //return;
-  }
-  
-
+  if(array_sum(irBinaryValue,0,15)>=7 ) { 		
+		// if (array_sum(irBinaryValue,2,13)>=7 && array_sum(irBinaryValue,0,1)<2 && array_sum(irBinaryValue,14,15)<2)
+		if (array_sum(irBinaryValue,2,13)>=7 )
+		{    //Detect coin box
+			Serial2.println("IR_COIN_BOX");
+			return IR_COIN_BOX;
+		}else
+		{
+			return	IR_NORMAL_LINE;
+		}
+		
+      
+    }
+    else
+    {
+      	return IR_NORMAL_LINE;
+    }
+    
 }
